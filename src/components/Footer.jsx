49f8b3573946aa1c62fcfa701d929ab54fa83024
huseyin <huseyinarslan06@gmail.com>
2022-06-30 17:30:39 +0300

    second step

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,13 @@
-
 const Footer = () => {
-    return (
-      <footer className="page-footer">
-        <div className="container">
-          <small>
-            Coded by <span>❤</span>FS Team
-          </small>
-        </div>
-      </footer>
-    );
-  };
-  
-  export default Footer;
+  return (
+    <footer className="page-footer">
+      <div className="container">
+        <small>
+          Have a Nice <span>❤</span> Day
+        </small>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
